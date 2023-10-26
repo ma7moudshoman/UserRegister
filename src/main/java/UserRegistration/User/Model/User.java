d95a9b7b9682331532a00984935e0e18608cac3a
ma7moudshoman <shomanm53@gmail.com>
2023-10-26 12:04:57 +0300

    usre

--- src/main/java/UserRegistration/User/Model/User.java
+++ src/main/java/UserRegistration/User/Model/User.java
@@ -17,6 +17,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+
   private String Name;
   private String street;
   private String City;
